9b1fa386f07db20b092f104c19991ae426dff7f2
Garnepudi Venkatesh <garnepudi.venkatesh@gmail.com>
2019-06-24 13:02:36 +0530

    Update DL-2.py

--- DL-2.py
+++ DL-2.py
@@ -6,6 +6,11 @@ mnist = tf.keras.datasets.fashion_mnist
 
 (train_x,train_y),(test_x,test_y) = mnist.load_data()
 
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>0.6):
+      print("\nReached 60% accuracy so cancelling training!")
+      self.model.stop_training = True
 #Step1
 
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
@@ -18,9 +23,9 @@ model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
 #step2
 
 model.compile(optimizer = "sgd", loss = "cross-entropy-loss")
-
+call_back = myCallback()
 #step3
-model.fit(train_x, train_y, epochs = 5)
+model.fit(train_x, train_y, epochs = 5, callbacks = [callback])
 
 #step4
 
