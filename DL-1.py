75e666c51e54d9636be14c2c0cb43de5839c3074
Garnepudi Venkatesh <garnepudi.venkatesh@gmail.com>
2019-06-28 11:54:30 +0530

    Update DL-1.py

--- DL-1.py
+++ DL-1.py
@@ -13,7 +13,7 @@ import numpy as np
 model = tf.keras.Sequential([Keras.layers.Dense(units = 1, input_shape = [1])])
 
 #2
-model.compile(optimizer = "sgd", loss = 'mean_squared_error)
+model.compile(optimizer = "sgd", loss = 'mean_squared_error')
 
 xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
