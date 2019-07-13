e124fd058445205bb95172fa37a2d2a2b5d941b9
Garnepudi Venkatesh <garnepudi.venkatesh@gmail.com>
2019-07-13 13:10:14 +0530

    Update DL-6.py

--- DL-6.py
+++ DL-6.py
@@ -14,3 +14,40 @@ model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(16, (3,3), input_shap
                                     tf.keras.layers.Dense(1, activation = 'sigmoid')
                                     
                                     ])
+
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(optimizer=RMSprop(lr=0.001),
+              loss='binary_crossentropy',
+              metrics = ['acc'])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by 1./255.
+train_datagen = ImageDataGenerator( rescale = 1.0/255. )
+test_datagen  = ImageDataGenerator( rescale = 1.0/255. )
+
+# --------------------
+# Flow training images in batches of 20 using train_datagen generator
+# --------------------
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(300,300))     
+# --------------------
+# Flow validation images in batches of 20 using test_datagen generator
+# --------------------
+validation_generator =  test_datagen.flow_from_directory(validation_dir,
+                                                         batch_size=20,
+                                                         class_mode  = 'binary',
+                                                         target_size = (300,300))
+
+
+#training...
+history = model.fit_generator(train_generator,
+                              validation_data=validation_generator,
+                              steps_per_epoch=100,
+                              epochs=10,
+                              validation_steps=50,
+                              verbose=2)
