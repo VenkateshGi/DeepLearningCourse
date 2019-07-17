7236827764757e8b84e7fb92e907b280f1536a35
Garnepudi Venkatesh <garnepudi.venkatesh@gmail.com>
2019-07-17 12:39:15 +0530

    Create DL-7.py

---
+++ DL-7.py
@@ -0,0 +1,26 @@
+#for extracting the dataset from zip file
+import os
+import zipfile
+
+local_zip = '/tmp/horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/horse-or-human')
+local_zip = '/tmp/validation-horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/validation-horse-or-human')
+zip_ref.close()
+# Directory with our training horse pictures
+train_horse_dir = os.path.join('/tmp/horse-or-human/horses')
+
+# Directory with our training human pictures
+train_human_dir = os.path.join('/tmp/horse-or-human/humans')
+
+# Directory with our training horse pictures
+validation_horse_dir = os.path.join('/tmp/validation-horse-or-human/horses')
+
+# Directory with our training human pictures
+validation_human_dir = os.path.join('/tmp/validation-horse-or-human/humans')
+
+
+##########
+
