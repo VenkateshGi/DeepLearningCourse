acac677fedf5eb3fff8d7ff4ab56127da95f309c
Garnepudi Venkatesh <garnepudi.venkatesh@gmail.com>
2019-07-17 15:08:42 +0530

    Update DL-7.py

--- DL-7.py
+++ DL-7.py
@@ -23,4 +23,33 @@ validation_human_dir = os.path.join('/tmp/validation-horse-or-human/humans')
 
 
 ##########
+#Let's create the model now
+import tensorflow as tf
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(16,(3,3),input_shape = (300,300,3), activation = 'relu'),
+  tf.keras.layers.Maxpooling2D(2,2),
+  tf.keras.layers.Conv2D(32,(3,3),activation = 'relu'),
+  tf.keras.layers.Maxpooling2D(2,2),
+  tf.keras.layers.Conv2D(64,(3,3),activation = 'relu'),
+  tf.keras.layers.Maxpooling2D(2,2),
+  tf.keras.layers.Conv2D(64,(3,3),activation = 'relu'),
+  tf.keras.layers.Maxpooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation = 'relu')
+  tf.keras.layers.Dense(1, activation = 'sigmoid')
+])
 
+############
+#compiling the model
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+###
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale = 1/255,
+                                   rotation_range = 40,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True,
+                                   fill_mode = 'nearest')
